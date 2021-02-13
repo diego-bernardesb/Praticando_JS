8590aae2a8258c0a9c9562bca23d14c8a6c343ad
Diego Bernardes <diegobbabo@gmail.com>
2021-02-13 17:05:37 -0300

    Adicionado objto para receber cadastro do formulario

--- form_app.js
+++ form_app.js
@@ -1,20 +1,32 @@
-const nome = document.getElementById('nome')
-const sobName = document.getElementById('sobName')
-const idade = document.getElementById('idade')
-const email = document.getElementById('e-mail')
-const cpf = document.getElementById('cpf')
-const cnh = document.getElementById('cnh')
-const rg = document.getElementById('rg')
-const genero = document.getElementById('genero')
-const ativo = document.getElementById('ativo')
+const nome = document.getElementById('nome').value
+const sobNome = document.getElementById('sobName').value
+const idade = document.getElementById('idade').value
+const email = document.getElementById('e-mail').value
+const cpf = document.getElementById('cpf').value
+const cnh = document.getElementById('cnh').value
+const rg = document.getElementById('rg').value
+const genero = document.getElementById('genero').value
+const ativo = document.getElementById('ativo').value
 const resultado = document.getElementById('div#res')
 const nomeUser = [];
 
+
 function enviar(){
-    if(nomeUser.length == 0){//confere todos os campos estão preenchidos
-        alert('Preencha todos os campos para concluir o registro!')
-    }else{//resultado apresentado na tela.
+    //resultado apresentado na tela.
+
+        let newUser = {
+            name: nome,
+            sobName: sobNome,
+            email: email,
+            cpf: cpf,
+            cnh: cnh,
+            rg: rg,
+            genero: genero,
+            ativo: ativo
+        }
+        newUser.push(nomeUser);
+
         resultado.innerHTML = ''
         resultado.innerHTML +=`Eu ${nome} ${sobName} do genero ${genero} com ${idade} anos de idade e portador do CPF ${cpf}, e do registro geral ${rg} e da carteira de habilitação ${cnh} concordo em registrar o e-mail ${email} e sendo um membro ${ativo} para estar recebendo de participando das discussões para o desenvolvimento do grupo.`
-    }
+    
 }
