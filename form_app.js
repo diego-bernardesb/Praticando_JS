603f9d1b68c8b7c60476ebb76162632a5d838d70
Diego Bernardes <diegobbabo@gmail.com>
2021-02-13 19:55:51 -0300

    Realizado as correções de texto e aplicado mensagem de regristro completo

--- form_app.js
+++ form_app.js
@@ -1,17 +1,18 @@
-const nome = document.getElementById('nome').value
-const sobNome = document.getElementById('sobName').value
-const idade = document.getElementById('idade').value
-const email = document.getElementById('e-mail').value
-const cpf = document.getElementById('cpf').value
-const cnh = document.getElementById('cnh').value
-const rg = document.getElementById('rg').value
-const genero = document.getElementById('genero').value
-const ativo = document.getElementById('ativo').value
-const resultado = document.getElementById('div#res')
-const nomeUser = [];
-
 
 function enviar(){
+    //Variaveis com valores recebido do formulario
+    const nome = document.getElementById('nome').value
+    const sobNome = document.getElementById('sobName').value
+    const idade = document.getElementById('idade').value
+    const email = document.getElementById('e-mail').value
+    const cpf = document.getElementById('cpf').value
+    const cnh = document.getElementById('cnh').value
+    const rg = document.getElementById('rg').value
+    const genero = document.getElementById('genero').value
+    const ativo = document.getElementById('ativo').value
+    const resultado = document.getElementById('res')
+    const nomeUser = [];
+
     //resultado apresentado na tela.
 
         let newUser = {
@@ -24,9 +25,8 @@ function enviar(){
             genero: genero,
             ativo: ativo
         }
-        newUser.push(nomeUser);
+        nomeUser.push(newUser);
 
         resultado.innerHTML = ''
-        resultado.innerHTML +=`Eu ${nome} ${sobName} do genero ${genero} com ${idade} anos de idade e portador do CPF ${cpf}, e do registro geral ${rg} e da carteira de habilitação ${cnh} concordo em registrar o e-mail ${email} e sendo um membro ${ativo} para estar recebendo de participando das discussões para o desenvolvimento do grupo.`
-    
+        resultado.innerHTML +=`Eu <b>${newUser.name} ${sobNome}</b> do sexo <b>${genero}</b> com <b>${idade}</b> anos de idade e portador do CPF <b>${cpf}</b>, e do registro geral <b>${rg}</b> e da carteira de habilitação <b>${cnh}</b> concordo em registrar o e-mail <b>${email}</b> e sendo um membro <b>${ativo}</b> para estar recebendo de participando das discussões para o desenvolvimento do grupo.`
 }
